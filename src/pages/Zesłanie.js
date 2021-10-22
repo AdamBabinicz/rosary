23c20d1c31a65663a7597403ee316873bbc3c83a
babinicz <babinicz@onet.pl>
2021-10-22 15:34:15 +0200

    skrót

--- src/pages/Zesłanie.js
+++ src/pages/Zesłanie.js
@@ -27,27 +27,9 @@ const Pricing = () => {
           dzięki mocy Ducha Świętego, który w nim działa. Maryja jak na początku
           tak i dziś umacnia nas w miłości do Kościoła, coraz głębiej wprowadza
           nas w jego życie a przez to prowadzi nas do swego Syna, do Ducha
-          Świętego i do Boga Ojca. Swą matczyną troską otacza tą największą
-          Chrystusową spuściznę. Maryjo upraszaj nam dary Ducha Świętego abyśmy
-          byli przykładem żywej wiary, dobrymi dziećmi Kościoła i abyśmy nie
-          ustawali w poznawaniu Boga.
+          Świętego i do Boga Ojca.
         </p>
-        {/* <p
-          style={{
-            fontSize: "15px",
-            lineHeight: "1.8rem",
-            textTransform: "none",
-            textAlign: "left",
-            fontWeight: "lighter",
-            marginBottom: "1rem",
-          }}
-        >
-          Bo jest jedną z nas. Jej życie na ziemi nie było pozbawione ludzkich
-          problemów: zwykłych trosk, cierpienia, udręk, bólu, pomimo tego (a
-          może właśnie, dlatego), że była Matką Boga, a więc tak blisko Niego.
-          Musiała tak wiele wycierpieć. I cierpiała, lecz jak? W łączności z
-          Bogiem, bo tylko takie cierpienie ma sens.
-        </p> */}
+
         <Link
           to="/locations"
           style={{
